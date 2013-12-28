1ce1b19410fadcc1f8d012dcea3342562b92d39f
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-12-29 03:02:50 +0800

    faster...

--- src/imgDist.cc
+++ src/imgDist.cc
@@ -1,5 +1,5 @@
 //File: imgDist.cc
-//Date: Sun Dec 29 02:41:40 2013 +0800
+//Date: Sun Dec 29 03:00:30 2013 +0800
 //Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 #include <limits>
@@ -32,11 +32,11 @@ namespace {
 		return ret;
 	}
 
-	inline Color get_patch_point(const Img& img, Patch p, int w, int h) {
-		return img.get_pixel(p.second + h, p.first + w);
-	}
+	inline Color get_patch_point(const Img& img, Patch p, int w, int h)
+	{ return img.get_pixel(p.second + h, p.first + w); }
 
-	real_t cal_IMED(const Img& img1, Patch p1, const Img& img2, Patch p2) {
+	real_t cal_IMED(const Img& img1, Patch p1, const Img& img2, Patch p2, real_t thres) {
+		thres = thres * thres;
 		real_t start_diff = sqr((float)p1.first / img1.w - (float)p2.first / img2.w) + sqr((float)p1.second / img1.h - (float)p2.second / img2.h);
 		if (start_diff > 0.05)
 			return numeric_limits<real_t>::max();
@@ -45,35 +45,34 @@ namespace {
 		REP(i1, PATCH_SIZE) REP(j1, PATCH_SIZE) REP(i2, PATCH_SIZE) REP(j2, PATCH_SIZE) {
 			real_t diff_1 = (get_patch_point(img1, p1, i1, j1) - get_patch_point(img2, p2, i1, j1)).sqr(),
 				   diff_2 = (get_patch_point(img1, p1, i2, j2) - get_patch_point(img2, p2, i2, j2)).sqr();
-			real_t dij2 = sqr((float)i1 / img1.w - (float)i2 / img2.w) + sqr((float)j1 / img1.h - (float)j2 / img2.h),
-				   gij = exp(-dij2) / 2 / M_PI;
+			//real_t dij2 = sqr((float)i1 / img1.w - (float)i2 / img2.w) + sqr((float)j1 / img1.h - (float)j2 / img2.h);
+		    //real_t gij = exp(-dij2) / 2 / M_PI;
+			real_t gij = 1 / start_diff;		/// much faster than exp
 			sum += gij * diff_1 * diff_2;
+			if (sum > thres) return numeric_limits<real_t>::max();
 		}
 		return sqrt(sum);
 	}
 
 	real_t cal_dIE(const Img& img1, const vector<Patch>& p1, const Img& img2, const vector<Patch>& p2) {
 		Matrix m(p1.size(), p2.size());
+		real_t row_min[m.h],
+			   col_min[m.w];
+		REP(i, m.h) row_min[i] = numeric_limits<real_t>::max();
+		REP(i, m.w) col_min[i] = numeric_limits<real_t>::max();
 		REP(i, m.h)  {
-			PP(i);
-			REP(j, m.w)
-				m.get(i, j) = cal_IMED(img2, p2[i], img1, p1[j]);
+			REP(j, m.w) {
+				real_t ret = cal_IMED(img2, p2[i], img1, p1[j], max(row_min[i], col_min[j]));
+				m.get(i, j) = ret;
+				update_min(row_min[i], ret);
+				update_min(col_min[j], ret);
+			}
 		}
 
-		real_t sum0 = 0;
-		REP(i, m.w) {		// every column
-			real_t min_d = numeric_limits<real_t>::max();
-			REP(j, m.h) update_min(min_d, m.get(i, j));
-			sum0 += min_d;
-		}
+		real_t sum0 = 0, sum1 = 0;
+		REP(j, m.w) sum0 += col_min[j];
 		sum0 /= m.w;
-
-		real_t sum1 = 0;
-		REP(j, m.h) {
-			real_t min_d = numeric_limits<real_t>::max();
-			REP(i, m.w) update_min(min_d, m.get(i, j));
-			sum1 += min_d;
-		}
+		REP(i, m.h) sum1 += row_min[i];
 		sum1 /= m.h;
 		return sum0 + sum1;
 	}
